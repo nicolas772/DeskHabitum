3984c8bd5fd9f09548ba3ec48a0ecaf66b0e572b
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-06-13 22:02:57 -0400

    sidebar oculta correctamente main

--- src/views/app.js
+++ src/views/app.js
@@ -2,4 +2,11 @@ const shrink_btn = document.querySelector(".shrink-btn");
 
 shrink_btn.addEventListener("click",()=>{
     document.body.classList.toggle("shrink");
-})
+    shrink_btn.classList.add("hovered");
+
+    setTimeout(
+        ()=>{
+            shrink_btn.classList.remove("hovered");
+        },500);
+
+});
