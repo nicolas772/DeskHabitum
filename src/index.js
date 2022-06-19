c940b7a91ce2560cbc76d0e6a2b398c41c6be649
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2022-06-19 14:26:37 -0400

    fix problema unhas-sesion

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const createWindow = () => {
         preload: path.join(__dirname, './preload.js')
       }
     })
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
     win.loadFile('src/views/index.html');
 }
 
