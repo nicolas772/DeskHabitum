772e762325bb164dce64131d4e82490c1009cd3c
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-06-21 12:07:09 -0400

    reportes onicofagia estaticos ordenados

--- src/app.js
+++ src/app.js
@@ -197,4 +197,68 @@ var conteounhas2 =window.api.countUnhasSesion(1).then(result => {
 
 //document.getElementById("h1totunhas").innerHTML=window.api.countAllUnhas(1);
 
+/*-------------------------Graficos onicofagia*/
 
+
+var options61 = {
+  series: [24, 35],
+  chart: {
+    width: 380,
+    type: 'pie',
+  },
+  labels: ['Tiempo Onicofagia', 'Tiempo Óptimo'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 300
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+};
+
+var chart61 = new ApexCharts(document.querySelector("#chart61"), options61);
+chart61.render();
+
+var options62 = {
+  series: [{
+    name: "% mordiendo uñas",
+    data: [13, 10, 41, 35, 51, 49, 62, 69, 75, 51]
+  }],
+  chart: {
+    height: 240,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: '% onicofagia últimas 10 sesiones.',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['1','2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  }
+};
+
+var chart62 = new ApexCharts(document.querySelector("#chart62"), options62);
+chart62.render();
+
+
+
+/*-----------------Fin graficos onicofagia*/
