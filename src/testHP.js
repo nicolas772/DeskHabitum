485a98f536a64a7f4765635adf484b63108efac1
Álvaro Fabián Ortiz Hermosilla <alvaro.ortizh@sansano.usm.cl>
2022-09-06 23:50:04 -0300

    Implementacion onicofagia

--- src/testHP.js
+++ src/testHP.js
@@ -55,111 +55,123 @@ async function loop() {
 // run the webcam image through the image model
 async function predict() {
     // predict can take in an image, video or canvas html element
-    let posesHand, posesBlaze, centroX, centroY, radio;
+    let posesHand, posesBlaze, centroX, centroY;
+    //https://github.com/tensorflow/tfjs-models/tree/master/hand-pose-detection
     posesHand = await detectorHand.estimateHands(webcam.canvas);
+    //https://github.com/tensorflow/tfjs-models/tree/master/pose-detection
     posesBlaze = await detectorBlaze.estimatePoses(webcam.canvas);
+
     
-    if (posesBlaze.length != 0){
-        boca1 = posesBlaze[0].keypoints[9]
-        boca2 = posesBlaze[0].keypoints[10]
-        boca13D = posesBlaze[0].keypoints3D[9]
-        boca23D = posesBlaze[0].keypoints3D[10]
-    
-        centroX = (boca1.x + boca2.x) / 2.0
-        centroY = (boca1.y + boca2.y) / 2.0
-        centroZ = (boca13D.z + boca23D.z) / 2.0 
-        coef = 10
+    //Los modelos estiman articulaciones
+    if (posesBlaze.length != 0 && posesHand.length != 0 && (posesBlaze[0].keypoints3D[19].score >= 0.8 || posesBlaze[0].keypoints3D[20].score >= 0.8)){
+
+        //Se consideran los puntos, del estimador BlazePose, que serán de utilidad. En este caso los dos puntos de la boca, en coordenadas (x,y) y (x,y,z). Y los puntos de los indices de la manos en 3d.
+        bocaLeft = posesBlaze[0].keypoints[9]
+        bocaRight = posesBlaze[0].keypoints[10]
+
+        bocaLeft3D = posesBlaze[0].keypoints3D[9]
+        bocaRight3D = posesBlaze[0].keypoints3D[10]
+
+        //Se define el centro de la boca en base a las coordenadas estimadas
+        centroX = (bocaLeft.x + bocaRight.x) / 2.0
+        centroY = (bocaLeft.y + bocaRight.y) / 2.0
+
+        //Se define un radio de detección desde el centro de la boca hasta el coef definido 
+        coef = 10;
 
         radioXUp = centroX + coef
         radioXLow = centroX - coef
 
         radioYUp = centroY - coef
         radioYLow = centroY + coef
-    }
 
-
-    
-    
-    //Solo se detecta una mano
-    if (posesHand.length == 1){
-        
-
-
+        //Se consideran las articulaciones interfalángicas distales de la mano detectada
         dipPulgar = posesHand[0].keypoints[3]
         dipIndice = posesHand[0].keypoints[7]
         dipMedio = posesHand[0].keypoints[11]
         dipAnular = posesHand[0].keypoints[15]
         dipMenique = posesHand[0].keypoints[19]
 
-
+        //Tambien las puntas de los dedos
         tipPulgar = posesHand[0].keypoints[4]
+        tipPulgar3D = posesHand[0].keypoints3D[4]
+
         tipIndice = posesHand[0].keypoints[8]
+        tipIndice3D = posesHand[0].keypoints3D[8]
+
         tipMedio = posesHand[0].keypoints[12]
+        tipMedio3D = posesHand[0].keypoints3D[12]
+
         tipAnular = posesHand[0].keypoints[16]
+        tipAnular3D = posesHand[0].keypoints3D[16]
+
         tipMenique = posesHand[0].keypoints[20]
-
-
+        tipMenique3D = posesHand[0].keypoints3D[20]
         
-        if ( tipPulgar.y > dipPulgar.y || tipIndice.y > dipIndice.y || tipMedio.y > dipMedio.y ||tipAnular.y > dipAnular.y || tipMenique.y > dipMenique.y){
-            if ((tipPulgar.x <= radioXUp && tipPulgar.x >= radioXLow && tipPulgar.y >= radioYUp && tipPulgar.y <= radioYLow) || (tipIndice.x <= radioXUp && tipIndice.x >= radioXLow && tipIndice.y >= radioYUp && tipIndice.y <= radioYLow)||(tipMedio.x <= radioXUp && tipMedio.x >= radioXLow && tipMedio.y >= radioYUp && tipMedio.y <= radioYLow) ||(tipAnular.x <= radioXUp && tipAnular.x >= radioXLow && tipAnular.y >= radioYUp && tipAnular.y <= radioYLow) ||(tipMenique.x <= radioXUp && tipMenique.x >= radioXLow && tipMenique.y >= radioYUp && tipMenique.y <= radioYLow) ){
-                console.log("¡¡COMIENDO UÑAS!!");
-            }
-            
+
+        if (tipPulgar.y > dipPulgar.y && tipPulgar.x <= radioXUp && tipPulgar.x >= radioXLow && tipPulgar.y >= radioYUp && tipPulgar.y <= radioYLow && tipPulgar3D.z > 0){
+            console.log("Comiendo uña pulgar");
+        }
+        else if (tipIndice.y > dipIndice.y && tipIndice.x <= radioXUp && tipIndice.x >= radioXLow && tipIndice.y >= radioYUp && tipIndice.y <= radioYLow && tipIndice3D.z > 0){
+            console.log("Comiendo uña indice");
+        }
+        else if (tipMedio.y > dipMedio.y && tipMedio.x <= radioXUp && tipMedio.x >= radioXLow && tipMedio.y >= radioYUp && tipMedio.y <= radioYLow && tipMedio3D.z > 0){
+            console.log("Comiendo uña medio");
         }
 
-    //Falta implementar lo mismo de antes pero para ambos casos
-    }else if (posesHand.length == 2){
-
-
-
-        dipPulgar = posesHand[0].keypoints[3]
-        dipIndice = posesHand[0].keypoints[7]
-        dipMedio = posesHand[0].keypoints[11]
-        dipAnular = posesHand[0].keypoints[15]
-        dipMenique = posesHand[0].keypoints[19]
-
-
-        tipPulgar = posesHand[0].keypoints[4]
-        tipIndice = posesHand[0].keypoints[8]
-        tipMedio = posesHand[0].keypoints[12]
-        tipAnular = posesHand[0].keypoints[16]
-        tipMenique = posesHand[0].keypoints[20]
-
-
-        dipPulgar2 = posesHand[1].keypoints[3]
-        dipIndice2 = posesHand[1].keypoints[7]
-        dipMedio2 = posesHand[1].keypoints[11]
-        dipAnular2 = posesHand[1].keypoints[15]
-        dipMenique2 = posesHand[1].keypoints[19]
-
-        tipPulgar2 = posesHand[1].keypoints[4]
-        tipIndice2 = posesHand[1].keypoints[8]
-        tipMedio2 = posesHand[1].keypoints[12]
-        tipAnular2 = posesHand[1].keypoints[16]
-        tipMenique2 = posesHand[1].keypoints[20]
-
-
-        
-        if ( tipPulgar.y > dipPulgar.y || tipIndice.y > dipIndice.y || tipMedio.y > dipMedio.y ||tipAnular.y > dipAnular.y || tipMenique.y > dipMenique.y){
-            if ((tipPulgar.x <= radioXUp && tipPulgar.x >= radioXLow && tipPulgar.y >= radioYUp && tipPulgar.y <= radioYLow) || (tipIndice.x <= radioXUp && tipIndice.x >= radioXLow && tipIndice.y >= radioYUp && tipIndice.y <= radioYLow)||(tipMedio.x <= radioXUp && tipMedio.x >= radioXLow && tipMedio.y >= radioYUp && tipMedio.y <= radioYLow) ||(tipAnular.x <= radioXUp && tipAnular.x >= radioXLow && tipAnular.y >= radioYUp && tipAnular.y <= radioYLow) ||(tipMenique.x <= radioXUp && tipMenique.x >= radioXLow && tipMenique.y >= radioYUp && tipMenique.y <= radioYLow) ){
-                console.log("¡¡COMIENDO UÑAS!!");
-            }
-            
-        }
-        else if ( tipPulgar2.y > dipPulgar2.y || tipIndice2.y > dipIndice2.y || tipMedio2.y > dipMedio2.y ||tipAnular2.y > dipAnular2.y || tipMenique2.y > dipMenique2.y){
-            if ((tipPulgar2.x <= radioXUp && tipPulgar2.x >= radioXLow && tipPulgar2.y >= radioYUp && tipPulgar2.y <= radioYLow) || (tipIndice2.x <= radioXUp && tipIndice2.x >= radioXLow && tipIndice2.y >= radioYUp && tipIndice2.y <= radioYLow)||(tipMedio2.x <= radioXUp && tipMedio2.x >= radioXLow && tipMedio2.y >= radioYUp && tipMedio2.y <= radioYLow) ||(tipAnular2.x <= radioXUp && tipAnular2.x >= radioXLow && tipAnular2.y >= radioYUp && tipAnular2.y <= radioYLow) ||(tipMenique2.x <= radioXUp && tipMenique2.x >= radioXLow && tipMenique2.y >= radioYUp && tipMenique2.y <= radioYLow) ){
-                console.log("¡¡COMIENDO UÑAS!!");
-            }
-            
+        else if (tipAnular.y > dipAnular.y && tipAnular.x <= radioXUp && tipAnular.x >= radioXLow && tipAnular.y >= radioYUp && tipAnular.y <= radioYLow && tipAnular3D.z > 0){
+            console.log("Comiendo uña anular");
         }
 
-    //No se detectan manos 
-    }else{
+        else if (tipMenique.y > dipMenique.y && tipMenique.x <= radioXUp && tipMenique.x >= radioXLow && tipMenique.y >= radioYUp && tipMenique.y <= radioYLow && tipMenique3D.z > 0 ){
+            console.log("Comiendo uña meñique");
+        }
 
-        console.log("No se detectan manos");
+        if (posesHand.length == 2){
 
+            dipPulgar2 = posesHand[0].keypoints[3]
+            dipIndice2 = posesHand[0].keypoints[7]
+            dipMedio2 = posesHand[0].keypoints[11]
+            dipAnular2 = posesHand[0].keypoints[15]
+            dipMenique2 = posesHand[0].keypoints[19]
+
+            //Tambien las puntas de los dedos
+            tipPulgar2 = posesHand[0].keypoints[4]
+            tipPulgar2_3D = posesHand[0].keypoints3D[4]
+
+            tipIndice2 = posesHand[0].keypoints[8]
+            tipIndice2_3D = posesHand[0].keypoints3D[8]
+
+            tipMedio2 = posesHand[0].keypoints[12]
+            tipMedio2_3D = posesHand[0].keypoints3D[12]
+
+            tipAnular2 = posesHand[0].keypoints[16]
+            tipAnular2_3D = posesHand[0].keypoints3D[16]
+
+            tipMenique2 = posesHand[0].keypoints[20]
+            tipMenique2_3D = posesHand[0].keypoints3D[20]
+
+
+            if (tipPulgar2.y > dipPulgar2.y && tipPulgar2.x <= radioXUp && tipPulgar2.x >= radioXLow && tipPulgar2.y >= radioYUp && tipPulgar2.y <= radioYLow && tipPulgar2_3D.z > 0){
+                console.log("Comiendo uña pulgar");
+            }
+            else if (tipIndice2.y > dipIndice2.y && tipIndice2.x <= radioXUp && tipIndice2.x >= radioXLow && tipIndice2.y >= radioYUp && tipIndice2.y <= radioYLow && tipIndice2_3D.z > 0){
+                console.log("Comiendo uña indice");
+            }
+            else if (tipMedio2.y > dipMedio2.y && tipMedio2.x <= radioXUp && tipMedio2.x >= radioXLow && tipMedio2.y >= radioYUp && tipMedio2.y <= radioYLow && tipMedio2_3D.z > 0){
+                console.log("Comiendo uña medio");
+            }
+
+            else if (tipAnular2.y > dipAnular2.y && tipAnular2.x <= radioXUp && tipAnular2.x >= radioXLow && tipAnular2.y >= radioYUp && tipAnular2.y <= radioYLow && tipAnular2_3D.z > 0){
+                console.log("Comiendo uña anular");
+            }
+
+            else if (tipMenique2.y > dipMenique2.y && tipMenique2.x <= radioXUp && tipMenique2.x >= radioXLow && tipMenique2.y >= radioYUp && tipMenique2.y <= radioYLow && tipMenique2_3D.z > 0 ){
+                console.log("Comiendo uña meñique");
+            }
+
+        }
     }
-
 }
 
 
