cd0547958d5e10e03ec1b38c524f426a95d6b507
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-09-11 19:48:13 -0300

    dejar index.html

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const createWindow = () => {
 
     //win.webContents.openDevTools();
     //win.loadFile('src/views/index.html');
-    win.loadFile('src/views/login2.html');
+    win.loadFile('src/views/index.html');
 }
 
 app.whenReady().then(() => {
