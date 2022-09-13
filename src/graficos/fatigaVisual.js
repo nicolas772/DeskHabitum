6d4c5865646750e1f85d423b42f0a3e8dcd03633
ithil0 <fernanda.rubio@sansano.usm.cl>
2022-09-13 16:03:43 -0300

    .

--- src/graficos/fatigaVisual.js
+++ src/graficos/fatigaVisual.js
@@ -1,1 +1,29 @@
 let ID_USER = window.api.get_user_id("")
+
+/* Grafico para fatiga mensual */
+
+var fatiga_mes = {
+    series: [{
+    name: 'Pestañeo promedio',
+    data: [10,10,10,10,10,10,10,10]
+  }, {
+    name: 'Pestañeo mes actual',
+    data: [11, 14, 8, 8, 16, 11, 17,19]
+  }],
+    chart: {
+    height: 350,
+    type: 'area'
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  xaxis: {          
+    categories: ["1", "2", "3", "4", "5", "6", "7","8"]
+  },        
+  };
+
+  var chart_fatm = new ApexCharts(document.querySelector("#chart_fatm"), fatiga_mes);
+  chart_fatm.render();
