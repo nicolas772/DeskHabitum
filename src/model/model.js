ac6f691d0e6ca92781b140fcdf80462c67a75e80
jorgec <jorge.cabrerab@sansano.usm.cl>
2022-10-06 16:37:51 -0300

    querys solicitud union al grupo

--- src/model/model.js
+++ src/model/model.js
@@ -410,6 +410,8 @@ const createGrupo = async (id_lider, nombre) => {
 const getCodeGrupo = async (id_lider) => {
     let query = `select code from grupos where lider = ${id_lider}`;
     const res = await conexion.query(query)
+    const result = res.rows
+    return result[0]['code']
 }
 
 
@@ -420,9 +422,11 @@ const addParticipante = async (id_usuario, code) => {
     id_grupo = result[0]['id']
     let query2 = `UPDATE users SET grupo = ${id_grupo} where id = ${id_usuario}`;
     const res2 = await conexion.query(query2)
+    //Quitarlo de los pendientes
+    let query3 = `delete from pendientes where id_user = ${id_usuario} and code = '${code}'`
+    const res3 = await conexion.query(query3)
+
 }
-
-
 
 const quitarDelGrupo = async (id_usuario, id_grupo) => {
     let query = `update grupos set participantes = array_remove(participantes, ${id_usuario}) where id = ${id_grupo}`;
@@ -450,6 +454,20 @@ const getParticipantesGrupo = async (id_grupo) => {
     return result2
 }
 
+const solicitudUnirseGrupo = async (id_user, code) => {
+    let query = `insert into pendientes (id_user, code) values(${id_user}, '${code}')`;
+    const res = await conexion.query(query)
+}
+
+const getSolicitudesGrupo = async (code) => {
+    let query = `SELECT users.id, users.nombre FROM users INNER JOIN pendientes ON pendientes.id_user=users.id where code = '${code}'`;
+    const res = await conexion.query(query)
+    const result = res.rows
+    return result
+}
+
+
+
 module.exports = { getUsuarios , createUser, getUserData, createSesion, getSesion, lastSesion,
                  totalTimeSesions, countUnhasSesion, allSesionsUnhas, percentageTenSesionUnhas, totalSesionTimeUnhas, durationSesion, totalTimeUnhas, createUnhas,
                   validateUser, postConfig, getConfig, updateConfig, confirmMail,
@@ -458,4 +476,4 @@ module.exports = { getUsuarios , createUser, getUserData, createSesion, getSesio
               /*nuevas querys*/  peorSesionMorder, mejorSesionMorder, peorSesionPelo, mejorSesionPelo, percentageTenSesionMorder, percentageTenSesionPelo, 
               sesionesMesUnha, sesionesMesMorder, sesionesMesPelo, mejorMesUnhas,
               peorMesUnhas, mejorMesPelo, peorMesPelo, mejorMesMorder, peorMesMorder,
-              createGrupo, getCodeGrupo,  addParticipante, quitarDelGrupo, getParticipantesGrupo}
+              createGrupo, getCodeGrupo,  addParticipante, quitarDelGrupo, getParticipantesGrupo, solicitudUnirseGrupo, getSolicitudesGrupo}
