b2db0196f3ef9b61a66ad6e7b4afe0691724b9eb
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2022-10-10 13:52:31 -0300

    cambios pequeños en pomodor

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -3,8 +3,8 @@
 let workTittle = document.getElementById('work');
 let breakTittle = document.getElementById('break');
 
-let workTime = 25;
-let breakTime = 5;
+let workTime = 1;
+let breakTime = 1;
 
 let seconds = "00"
 
