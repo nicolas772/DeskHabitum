2c45e904de4592feebe36107e757b196a1e61b5a
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-10-11 22:09:55 -0300

    vistagraf2

--- src/graficos/pomodoro2.js
+++ src/graficos/pomodoro2.js
@@ -1,5 +1,63 @@
 
 //----------GRAFICO POMODORO
+var dataHabitosUltimoPomodoro= {
+  series: [44,33,22,10],
+  chart: {
+    height:300,
+    width:400,
+  type: 'donut',
+},
+labels: ['Tiempo Onicofagia', 'Tiempo Tricotilomanía', 'Tiempo Morder objetos', 'Tiempo Óptimo'],
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      height:200,
+
+    },
+    legend: {
+      position: 'bottom',
+      verticalAlign: 'bottom',
+      align:'left'
+    }
+  }
+}]
+};
+
+document.getElementById("charHabitosUltimoPomodoro").innerHTML = ''
+var charHabitosUltimoPomodoro = new ApexCharts(document.getElementById("charHabitosUltimoPomodoro"), dataHabitosUltimoPomodoro);
+charHabitosUltimoPomodoro.render();
+
+
+let ctx = document.getElementById('detecciones_comparacion').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Peor sesión', 'Última sesión', 'Mejor sesión'],
+            datasets: [{
+                label: 'Cantidad de detecciones',
+                data: [parseInt(55),parseInt(33),parseInt(3)],
+                backgroundColor: [
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rrgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(255, 99, 132)'
+                ],
+                borderWidth: 1
+            }]
+        }
+       
+    });
+
+
+
+
+
+/*
 let opt_pomodoro = {
     series: [{
     name: 'Manías',
@@ -52,3 +110,4 @@ let opt_pomodoro = {
 
   let chart_pomodoro = new ApexCharts(document.querySelector("#chart_pomodoro"), opt_pomodoro);
   chart_pomodoro.render();
+*/
