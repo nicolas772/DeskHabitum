1b8d668c4ddd8d6c4c30afe26ef477d0aac20d35
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-10-28 00:56:10 -0300

    grafico comparacion meses v1

--- src/graficos/postura.js
+++ src/graficos/postura.js
@@ -48,36 +48,48 @@ const chart_postura = new Chart(ctx_p, {
     decimalsInFloat: false,
   }
 });
-/*
-const chart_postura = new Chart(ctx_p, {
-type: 'bar',
-        data: {
-            labels: ['Peor sesión', 'Última sesión', 'Mejor sesión'],
-            datasets: [{
-                label: 'Cantidad de detecciones',
-                data: [5, 2, 3],// data: [parseInt(peor_ses_obj), parseInt(ultima_ses_morder),parseInt(mejor_ses_obj)]
-                backgroundColor: [
-                  'rgba(58, 198, 143, 0.2)', //'rgba(255, 205, 86, 0.2)'
-                  'rgba(58, 198, 143, 0.2)', //'rgba(75, 192, 192, 0.2)'
-                  'rgba(58, 198, 143, 0.2)' //'rrgba(255, 99, 132, 0.2)' 
-                ],
-                borderColor: [
-                    'rgb(0, 91, 82)', //'rgb(255, 205, 86)'
-                    'rgb(0, 91, 82)', //'rgb(75, 192, 192)'
-                    'rgb(0, 91, 82)' //'rgb(255, 99, 132)'
-                ],
-                borderWidth: 1,
-                yaxis:{
-                  decimalsInFloat: false,
-                }
-            }]
-        }
-       
-    });
-  
+
+//----
+const labels = ['Septiembre','Octubre','Noviembre'];
+const data_posmes = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80],
+    backgroundColor: [
+      
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(75, 192, 192, 0.2)'
+    ],
+    borderColor: [
+      
+      'rgb(75, 192, 192)',
+      'rgb(75, 192, 192)',
+      'rgb(75, 192, 192)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+const ctx_pmes = document.getElementById('chart_posturames').getContext('2d');
+const config_posmes = new Chart(ctx_pmes,{
+  type: 'bar',
+  data: data_posmes,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+});
 
 
-*/
+
+//----
+
+
 
 
 
