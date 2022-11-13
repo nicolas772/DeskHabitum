5399858d219e7b428cd4645c4930dd3c3bae94c4
Fabian Jimenez <fabian.jimenez.olivares@gmail.com>
2022-11-13 17:34:58 -0300

    arreglo primero ciclo

--- src/respiracion.js
+++ src/respiracion.js
@@ -2,6 +2,8 @@ let ID_USER = window.api.get_user_id("")
 
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+const start = document.getElementById('start-res');
+
 /*
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2; //3s
@@ -19,15 +21,15 @@ const totalTime = 14000;
 const breatheTime = 7000; //para 10seg inhalacion 2 mantencion 5 exhalacion (al 7seg se le suman 3seg si agrego delaydelay para empezar)
 const holdTime = 2000; 
 
+var empezo=0;
+/*start.addEventListener("click",breathAnimation());*/
 
-
-
-//breathAnimation();
+/*breathAnimation();*/
 
 function breathAnimation() {
   text.innerText = 'INHALA';
   container.className = 'container grow';
-
+  
   setTimeout(() => {
     text.innerText = 'MANTÉN';
     container.className = 'container hold';
@@ -37,7 +39,8 @@ function breathAnimation() {
       container.className = 'container shrink2';
     }, holdTime);
   }, breatheTime);
+
+  setInterval(breathAnimation, totalTime);
+  Footer
 }
 
-setInterval(breathAnimation, totalTime);
-Footer
