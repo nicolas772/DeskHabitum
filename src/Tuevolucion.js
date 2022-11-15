a1df0457c2205e0f80dfbe599f4d71f699aeb9ce
ithil0 <fernanda.rubio@sansano.usm.cl>
2022-11-14 23:25:36 -0300

    carrusel casi

--- src/Tuevolucion.js
+++ src/Tuevolucion.js
@@ -7,20 +7,30 @@ function actualizarCarousel(){
     for(let i=0; i< fotos.length; i++){
         if (i==0){
             let carouselItem = document.createElement('div');
-            carouselItem.className = "carousel-item active";
+            carouselItem.className = "carousel-item active";            
+            carouselItem.style.height ="400px";
+            carouselItem.style.overflow ="hidden";
+            carouselItem.style.width ="100%";
             let img = document.createElement('img');
             img.className = "d-block w-100";
             img.src = "../images/unhasUser/"+fotos[i]
             img.alt = "..."
+            img.style.width = "100%";
+            img.style.overflow ="hidden";
             carouselItem.appendChild(img)
             carousel.appendChild(carouselItem)
         }else {
             let carouselItem = document.createElement('div');
-            carouselItem.className = "carousel-item";
+            carouselItem.className = "carousel-item";            
+            carouselItem.style.height ="400px";
+            carouselItem.style.overflow ="hidden";
+            carouselItem.style.width ="100%";
             let img = document.createElement('img');
             img.className = "d-block w-100";
             img.src = "../images/unhasUser/"+fotos[i]
             img.alt = "..."
+            img.style.width = "100%";
+            img.style.overflow ="hidden";
             carouselItem.appendChild(img)
             carousel.appendChild(carouselItem)
         }
